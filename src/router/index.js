c1c0808f93c5067fb4dab65df02852d333aac280
Julie J <jeannet.julie@gmail.com>
2021-04-15 20:33:22 +0200

    Création view Tasks  router vers cette view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Tasks from "../views/Tasks.vue";
 
 const routes = [
   {
@@ -14,8 +15,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/tasks",
+    name: "Tasks",
+    component: Tasks,
+  },
+
 ];
 
 const router = createRouter({
---
+++ src/views/Tasks.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><h3>Toutes les tâches</h3></div>
+</template>
+
+<script>
+export default {
+    name:'Tasks'
+};
+</script>
+
+<style></style>
