70b3f69e335241a0871e7b560437edd7615147e2
Julie J <jeannet.julie@gmail.com>
2021-04-16 08:12:41 +0200

    Afficher toutes les tâches existantes

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,11 +1,36 @@
 <template>
-  <div><h3>Toutes les tâches</h3></div>
+  <div v-if="tasks.length > 0">
+    <div class="task" v-for="task in tasks" :key="task.id">
+      <h3>{{ task.name }}</h3>
+      <p>{{ task.description }}</p>
+      <p>Echéance : {{ task.temporility }}</p>
+    </div>
+  </div>
 </template>
 
+// pour récupérer la liste des tâches
+// on utilise notre service > méthode read
 <script>
+    // ref permet de se créer une référence qui corresponde à toutes les tâches
+    // pour... boucler dessus ? 
+import { ref } from "vue";
+import tasksService from "@/services/tasks.js";
 export default {
-    name:'Tasks'
+  setup() {
+
+    const tasks = ref([]);
+    // le .value est nécessaire côté JS,  pas côté template
+    tasks.value = tasksService.read();
+// 
+    return { tasks };
+  },
 };
 </script>
 
-<style></style>
+<style>
+.task {
+  margin: 10px 15px;
+  border: 3px solid #42b983;
+  border-radius: 5px;
+}
+</style>  
