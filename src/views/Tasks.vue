82efaf5ae6354828b674eead10b9f5d27c636db5
Julie J <jeannet.julie@gmail.com>
2021-04-16 19:44:36 +0200

    récupération saisies utilisateur pour filtrer les tâches à afficher

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,33 +1,47 @@
 <template>
   <div v-if="tasks.length > 0">
-    <div class="task" v-for="task in tasks" :key="task.id">
-      <h3>{{ task.name }}</h3>
-      <p>{{ task.description }}</p>
-      <p>Echéance : {{ convertCase(task.temporality)}}</p>
+    <input type="text" placeholder="filtrer" v-model="letters" @keyup="filter"/>
+    <div v-show="tasksFiltered.length > 0">
+      <div class="task" v-for="task in tasksFiltered" :key="task.id">
+        <h3>{{ task.name }}</h3>
+        <p>{{ task.description }}</p>
+        <p>Echéance : {{ convertCase(task.temporality) }}</p>
+      </div>
     </div>
   </div>
 </template>
 
-// pour récupérer la liste des tâches
-// on utilise notre service > méthode read
+// pour récupérer la liste des tâches // on utilise notre service > méthode read
 <script>
-    // ref permet de se créer une référence qui corresponde à toutes les tâches
-    // pour... boucler dessus ? 
+// ref permet de se créer une référence qui corresponde à toutes les tâches
+// pour... boucler dessus ?
 import { ref } from "vue";
 import tasksService from "@/services/tasks.js";
 export default {
   setup() {
-
     const tasks = ref([]);
+    const letters = ref("");
+    let tasksFiltered = ref([]);
     // le .value est nécessaire côté JS,  pas côté template
     tasks.value = tasksService.read();
-    
+    filter();
+
     function convertCase(temporality) {
-          return tasksService.convertCase(temporality);
+      return tasksService.convertCase(temporality);
+    }
+
+    function filter() {
+      if (letters.value.length === 0) {
+        tasksFiltered.value = tasks.value;
+      } else {
+        tasksFiltered.value = tasks.value.filter((t) =>
+          t.name.toLocaleLowerCase().includes(letters.value.toLocaleLowerCase())
+        );
       }
-// comme à chaque fois, pour pouvoir l'utiliser au dessus
-// côté template, on "l'exporte"
-    return { tasks, convertCase };
+    }
+    // comme à chaque fois, pour pouvoir l'utiliser au dessus
+    // côté template, on "l'exporte"
+    return { tasks, letters, tasksFiltered, convertCase, filter };
   },
 };
 </script>
@@ -38,4 +52,4 @@ export default {
   border: 3px solid #42b983;
   border-radius: 5px;
 }
-</style>  
+</style>
