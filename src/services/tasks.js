75b5c398183f91de4e97c0ac44899cba61d5e029
Julie J <jeannet.julie@gmail.com>
2021-04-17 18:44:51 +0200

    faire persister dans localStorage avec création de 2 fonctions save + retrieveTasks

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,14 +1,27 @@
 // pour émuler le fait qu'on a une base de donnée
 // on crée un tableau qui aura autant d'objets que de tâches
 let tasks = [];
+const storageKey = 'tasks-vue3-compo-api';
 
 function create(task) {
+    // par sécurité ??
+    // if(tasks === null) {
+    //     tasks = [];
+    //   }
+
     tasks = [task, ...tasks];
     console.log('tasks', tasks);
+    save();
 }
 
 function read() {
+    tasks = retrieveTasks();
     return tasks;
+}
+
+function deleteTask(id) {
+    tasks = tasks.filter((t) => t.id !== id);
+    save();
 }
 
 function convertCase(temporalityKebabCase) {
@@ -30,13 +43,22 @@ function convertCase(temporalityKebabCase) {
     return result;
 }
 
-function deleteTask(id) {
-    tasks = tasks.filter((t) => t.id !== id);
-  }
+function save() {
+    localStorage.setItem(storageKey, JSON.stringify(tasks));
+}
+
+// à usage interne, on n'a pas besoin d'ajouter cette fonction au tableau des exports
+function retrieveTasks() {
+    // pour récupérer ce qui est récupéré depuis localStorage !
+    const fromLocalstorage = localStorage.getItem(storageKey)
+    return JSON.parse(fromLocalstorage);
+}
+
 
 export default {
     create,
     read,
     convertCase,
-    deleteTask
+    deleteTask,
+    save,
 };
