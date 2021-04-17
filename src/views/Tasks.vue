95aaf4f1c1a034dc521934f21d54e5464f81cf3b
Julie J <jeannet.julie@gmail.com>
2021-04-17 10:33:16 +0200

    créer et binder un groupe de boutons pour affiner les options de recherche puis Utiliser un watcher plugué sur la variable bindée

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,6 +1,46 @@
 <template>
   <div v-if="tasks.length > 0">
-    <input type="text" placeholder="filtrer" v-model="letters" @keyup="filter"/>
+    <!-- pour les boutons permettant de selectionner une temporality -->
+    <div class="radio-filters">
+      <label for="all">
+        <input type="radio" id="all" value="" v-model="selectedTemporality" />
+        toutes
+      </label>
+      <label for="short-term">
+        <input
+          type="radio"
+          id="short-term"
+          value="short-term"
+          v-model="selectedTemporality"
+        />
+        court terme
+      </label>
+      <label for="medium-term">
+        <input
+          type="radio"
+          id="medium-term"
+          value="medium-term"
+          v-model="selectedTemporality"
+        />
+        moyen terme
+      </label>
+      <label for="long-term">
+        <input
+          type="radio"
+          id="long-term"
+          value="long-term"
+          v-model="selectedTemporality"
+        />
+        long terme
+      </label>
+    </div>
+    <!-- fin -->
+    <input
+      type="text"
+      placeholder="filtrer"
+      v-model="letters"
+      @keyup="filter"
+    />
     <div v-show="tasksFiltered.length > 0">
       <div class="task" v-for="task in tasksFiltered" :key="task.id">
         <h3>{{ task.name }}</h3>
@@ -15,13 +55,18 @@
 <script>
 // ref permet de se créer une référence qui corresponde à toutes les tâches
 // pour... boucler dessus ?
-import { ref } from "vue";
+// permet également de créer une référence côté template pour les options du select
+// de manière à  pouvoir les observer !
+import { ref, watch } from "vue";
 import tasksService from "@/services/tasks.js";
 export default {
   setup() {
+    // console.log("selectedTemporality", selectedTemporality);
     const tasks = ref([]);
     const letters = ref("");
+    const selectedTemporality = ref("");
     let tasksFiltered = ref([]);
+
     // le .value est nécessaire côté JS,  pas côté template
     tasks.value = tasksService.read();
     filter();
@@ -30,6 +75,15 @@ export default {
       return tasksService.convertCase(temporality);
     }
 
+    // function filter() {
+    //   if (letters.value.length === 0) {
+    //     tasksFiltered.value = tasks.value;
+    //   } else {
+    //     tasksFiltered.value = tasks.value.filter((t) =>
+    //       t.name.toLocaleLowerCase().includes(letters.value.toLocaleLowerCase())
+    //     );
+    //   }
+    // }
     function filter() {
       if (letters.value.length === 0) {
         tasksFiltered.value = tasks.value;
@@ -38,10 +92,35 @@ export default {
           t.name.toLocaleLowerCase().includes(letters.value.toLocaleLowerCase())
         );
       }
+      // pour inclure les options de pré-sélection par type de temporality
+      if (selectedTemporality.value !== "") {
+        tasksFiltered.value = tasksFiltered.value.filter(
+          (t) => t.temporality === selectedTemporality.value
+        );
+      }
     }
-    // comme à chaque fois, pour pouvoir l'utiliser au dessus
-    // côté template, on "l'exporte"
-    return { tasks, letters, tasksFiltered, convertCase, filter };
+
+    watch(selectedTemporality, (newValue, oldValue) => {
+      console.log("newValue: ", newValue, "oldValue: ", oldValue);
+      if (newValue !== "") {
+        filter();
+      } else {
+        // take everything then filter
+        tasksFiltered.value = tasks.value;
+        filter();
+      }
+    });
+
+    // comme à chaque fois, pour pouvoir utiliser chaque variable côté template,
+    // on les "exporte"
+    return {
+      tasks,
+      letters,
+      selectedTemporality,
+      tasksFiltered,
+      convertCase,
+      filter,
+    };
   },
 };
 </script>
@@ -51,5 +130,9 @@ export default {
   margin: 10px 15px;
   border: 3px solid #42b983;
   border-radius: 5px;
+}
+.radio-filters {
+  display: flex;
+  justify-content: center;
 }
 </style>
